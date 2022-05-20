b80ec7783f20f214d4f88877f83af45e8f7354cd
Chester Asuncion <asuncionmcv@gmail.com>
2022-05-20 13:46:38 +0800

    break down user into components

--- weight-tracket-ui/src/User.js
+++ weight-tracket-ui/src/User.js
@@ -1,51 +1,121 @@
 import React, {Component, useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 
+function sentenceCase(string) {
+  return string[0].toUpperCase() + string.slice(1)
+}
+
+function TextAttributes(props) {
+  const { object, attribute, notEditable, onChange} = props
+
+  const handleChange = (e) => {
+    const attribute = e.target.getAttribute("id")
+    const value = e.target.value
+    onChange(attribute, value)
+  }
+
+  return(
+    <div>
+      <label htmlFor={attribute}>{sentenceCase(attribute)}: </label>
+      <input type="text" id={attribute} value={object[attribute]} 
+             disabled={notEditable} onChange={handleChange}
+      />
+    </div>
+  )
+}
+
+function NumberAttributes(props) {
+  const { object, attribute, notEditable, onChange } = props
+
+  const handleChange = (e) => {
+    const attribute = e.target.getAttribute("id")
+    const value = e.target.value
+    onChange(attribute, value)
+  }
+
+  return(
+    <div>
+      <label htmlFor={attribute}>{sentenceCase(attribute)}</label>
+      <input type="number" id={attribute} value={object[attribute]}
+             disabled={notEditable} onChange={handleChange}
+      />
+    </div>
+  )
+}
+
+function RadioAttributes(props) {
+  const { object, attribute, notEditable, choices, onClick} = props
+
+  const handleClick = (e) => {
+    const value = e.target.value
+    const attribute = e.target.getAttribute("name")
+
+    onClick(attribute, value)
+  }
+
+  return(
+    <div>
+      <label htmlFor={attribute}>{sentenceCase(attribute)}: </label>
+      {
+        choices.map((choice) => {
+          return (
+            <span key={choice}>
+              <input type="radio" id={`${attribute}_${choice}`}  value={choice}
+                      name={attribute} disabled={notEditable}
+                      checked={choice==object[attribute] ? true : false }
+                      onClick={handleClick}
+                      />
+              <label htmlFor={choice}>{sentenceCase(choice)}</label>
+            </span>
+          )
+        })
+      }
+    </div>
+  )
+}
+
 function User() {
   var {userId} = useParams(); 
   const [user, setUser] = useState({});
+
   useEffect(() => { 
     fetch(`http://localhost:8080/v1/api/user/${userId}`, {mode: "cors"})
     .then(response => response.json())
       .then(user => setUser(user))
-  })
+    
+  }, []);
+
+  const handleUserChange = (attribute, value) => {
+    var updated_user = user
+    updated_user[attribute] = value
+    setUser({...updated_user})
+  }
+
+  const [isNotEdit, setisNotEdit] = useState(true);
 
   return (
     <div>
       <h1>Hello {`${user.name}`}!</h1>
       <br></br>
+      <div>
+        {JSON.stringify(user)}
+      </div>
+      <br></br>
+      <button onClick={() => setisNotEdit(isNotEdit ? false : true) }>
+        {isNotEdit ? "Edit" : "Save" }
+      </button>
+
       <form>
-        <div>
-          <label for="name">Name: </label>
-          <input type="text" id="name" value={user.name} disabled={true}/>
-        </div>
-
-        <div>
-          <label for="email">Email: </label>
-          <input type="text" id="email" value={user.email} disabled={true}/>
-        </div>
-
-        <div>
-          <label for="age">Age: </label>
-          <input type="number" id="age" value={user.age} disabled={true}/>
-        </div>
-
-        <div>
-          <label for="height">Height: </label>
-          <input type="number" id="height" value={user.height} disabled={true}/>
-        </div>
-
-        <div>
-          <label for="sex">Sex: </label><br></br>
-          <input type="radio" id="male"  value="male" 
-                 name="sex" checked={user.sex=="male"} 
-                 disabled={true} />
-          <label for="male">Male</label>
-          <br></br>
-          <input type="radio" id="female"  value="female" 
-                 name="sex" checked={user.sex=="female"} disabled={true} />
-          <label for="female">Female</label>
-        </div>
+        <TextAttributes object={user} attribute="name" notEditable={isNotEdit} onChange={handleUserChange}/>
+        <TextAttributes object={user} attribute="email" notEditable={isNotEdit} onChange={handleUserChange}/>
+        <NumberAttributes object={user} attribute="age" notEditable={isNotEdit} onChange={handleUserChange}/>
+        <NumberAttributes object={user} attribute="height" notEditable={isNotEdit} onChange={handleUserChange}/>
+        <RadioAttributes 
+          object={user}
+          attribute="sex" 
+          notEditable={isNotEdit} 
+          choices={["male", "female"]}
+          onClick={handleUserChange} />
       </form>
     </div>
   )
