ae32675a87f66458505fd1aa6fdc71c7b5c73ae0
Chester Asuncion <asuncionmcv@gmail.com>
2022-06-17 21:55:41 +0800

    add handleError function
    not a handler function, but called internally in handlers to
    handle errors and add error json response to the context

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -1,10 +1,27 @@
 package app
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type Credentials struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type GenericResponse struct {
+	Status  bool        `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type AuthResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
 
 func (s *Server) ApiStatus() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -17,4 +34,33 @@ func (s *Server) ApiStatus() gin.HandlerFunc {
 
 		c.JSON(http.StatusOK, response)
 	}
+}
+
+// handles an error coming from a function in the api, which then
+// is considered an internal server error
+// or from a function within the handler which then maps to a Bad Request
+// this then adds a json response for the given gin context
+func handlerError(c *gin.Context, err error, source string) {
+	log.Printf("%v", err)
+
+	if source == "Request" {
+		c.JSON(
+			http.StatusBadRequest,
+			&GenericResponse{
+				Status:  false,
+				Message: "Bad Request",
+			},
+		)
+
+	} else if source == "Server" {
+		c.JSON(
+			http.StatusInternalServerError,
+			&GenericResponse{
+				Status:  false,
+				Message: "Internal Server Error",
+			},
+		)
+	}
+
+	return
 }
