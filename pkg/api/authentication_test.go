9f3a211b8f6971e601f86eb592b4ee057806a044
Chester Asuncion <asuncionmcv@gmail.com>
2022-06-20 21:40:08 +0800

    write the GenerateAccessToken Method test

--- pkg/api/authentication_test.go
+++ pkg/api/authentication_test.go
@@ -8,6 +8,10 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+const (
+	signingKey = "super_secret_hs256"
+)
+
 type Credentials struct {
 	Email    string
 	Password string
@@ -84,7 +88,7 @@ func TestValidateCredentials(t *testing.T) {
 
 	for _, test := range tests {
 		mockRepo := NewMockAuthRepo()
-		authService := api.NewAuthService(mockRepo)
+		authService := api.NewAuthService(mockRepo, signingKey)
 
 		t.Run(test.name, func(t *testing.T) {
 			validity, err := authService.ValidateCredentials(test.credentials.Email, test.credentials.Password)
@@ -101,11 +105,61 @@ func TestValidateCredentials(t *testing.T) {
 }
 
 func TestGenerateAccessToken(t *testing.T) {
-	// generates an access token giventhe credentials
+	// generates an access token given the credentials
+	tests := []struct {
+		name              string
+		email             string
+		expiration        int64
+		want_access_token string
+		want_err          error
+	}{
+		{
+			// https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZW1haWwiOiJleGlzdGluZ19lbWFpbEBlbWFpbC5jb20iLCJleHAiOjE2NTU2OTA1NzF9.KbC55juOe0dNX7DC7lT4vWaF-XhmNHXzi9UvqEQ1V1A
+			name:              "should return the right token given the following credentials v1",
+			email:             "existing_email@email.com",
+			expiration:        1655690571,
+			want_access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZW1haWwiOiJleGlzdGluZ19lbWFpbEBlbWFpbC5jb20iLCJleHAiOjE2NTU2OTA1NzF9.KbC55juOe0dNX7DC7lT4vWaF-XhmNHXzi9UvqEQ1V1A",
+			want_err:          nil,
+		},
+		{
+			// https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZW1haWwiOiJuZXdFbWFpbEBlbWFpbC5pbyIsImV4cCI6MTAxMTkwOTAxfQ.b5IhQ6pig8fqUiKxNt3LqP0Cs_21pDiHLG4U32TVJDo
+			name:              "should return the right token given the following credentials v2",
+			email:             "newEmail@email.io",
+			expiration:        101190901,
+			want_access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZW1haWwiOiJuZXdFbWFpbEBlbWFpbC5pbyIsImV4cCI6MTAxMTkwOTAxfQ.b5IhQ6pig8fqUiKxNt3LqP0Cs_21pDiHLG4U32TVJDo",
+			want_err:          nil,
+		},
+	}
 
-	// to complicated to test
+	for _, test := range tests {
+		mockRepo := NewMockAuthRepo()
+		authService := api.NewAuthService(mockRepo, signingKey)
+
+		t.Run(test.name, func(t *testing.T) {
+			generateAccessToken, err := authService.GenerateAccessToken(test.email, test.expiration)
+
+			if err != test.want_err {
+				t.Errorf("test %v failed. got %v, wanted: %v", test.name, err, test.want_err)
+			}
+
+			if generateAccessToken != test.want_access_token {
+				t.Errorf("test %v failed.\n\tgot: %v\n\twanted: %v", test.name, generateAccessToken, test.want_access_token)
+			}
+		})
+	}
+
 }
 
 func TestGenerateRefreshToken(t *testing.T) {
+	// generate a refresh token given the credentials
 
+	// probably mock how access tokens are generated
+	// just expect it to not return nil
+	// on credential entry
+}
+
+func TestValidateAccessToken(t *testing.T) {
+	// validate access token
+
+	//
 }
