e668d53320d1b56e1ab50288ce7b901e555a1a76
Chester Asuncion <asuncionmcv@gmail.com>
2022-06-30 11:25:02 +0800

    implement all database setup code

--- pkg/repository/storage_test.go
+++ pkg/repository/storage_test.go
@@ -2,10 +2,18 @@ package repository_test
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
+	"runtime"
 	"testing"
 	"weight-tracker/pkg/api"
+	"weight-tracker/pkg/repository"
+
+	"github.com/go-testfixtures/testfixtures/v3"
+	"github.com/golang-migrate/migrate/v4"
 )
 
 var (
@@ -20,17 +28,129 @@ const (
 	databaseName = "weight_tracker_test"
 )
 
+// formats the connection string
+func formatConnectionString() {
+	connection_string = fmt.Sprintf(
+		"postgres://%s:%s@%s/%s?sslmode=disable",
+		username, password, host, databaseName,
+	)
+}
+
+func connect_to_database(connection_string string) error {
+	var err error
+	db, err = sql.Open("postgres", connection_string)
+
+	if err != nil {
+		return err
+	}
+
+	if err = db.Ping(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func set_up_database(connection_string string) error {
+	if connection_string == "" {
+		err := errors.New("repository: the connString was empty")
+		return err
+	}
+
+	// get base path
+	_, b, _, _ := runtime.Caller(0)
+	basePath := filepath.Join(filepath.Dir(b), "../..")
+
+	migrationsPath := filepath.Join("file://", basePath, "/pkg/repository/migrations/")
+
+	m, err := migrate.New(migrationsPath, connection_string)
+
+	if err != nil {
+		return err
+	}
+
+	err = m.Up()
+
+	switch err {
+	case errors.New("no change"):
+		//
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func populate_database(db *sql.DB) error {
+	// use fixtures here
+
+	fixtures, err := testfixtures.New(
+		testfixtures.Database(db),
+		testfixtures.Dialect("postgres"),
+		testfixtures.Directory("testdata/fixtures"),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	if err = fixtures.Load(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func teardown_database(connection_string string) error {
+	// run down migrations instaed
+	if connection_string == "" {
+		err := errors.New("repository: the connString was empty")
+		log.Printf("Error on teardown: %v", err)
+		return err
+	}
+
+	// get base path
+	_, b, _, _ := runtime.Caller(0)
+	basePath := filepath.Join(filepath.Dir(b), "../..")
+
+	migrationsPath := filepath.Join("file://", basePath, "/pkg/repository/migrations/")
+
+	m, err := migrate.New(migrationsPath, connection_string)
+
+	if err != nil {
+		log.Printf("Error on teardown: %v", err)
+		return err
+	}
+
+	err = m.Drop()
+
+	switch err {
+	case errors.New("no change"):
+		//
+	default:
+		log.Printf("Error on teardown: %v", err)
+		return err
+	}
+
+	return nil
+}
+
 func TestMain(m *testing.M) {
+	var err error
 	formatConnectionString()
-	connect_to_database(connection_string)
-	set_up_database(connection_string)
-	populate_database(connection_string)
+	if err = connect_to_database(connection_string); err != nil {
+		return
+	}
+	if err = set_up_database(connection_string); err != nil {
+		return
+	}
+	if err = populate_database(db); err != nil {
+		return
+	}
+	defer teardown_database(connection_string)
 
 	exitVal := m.Run() // run all test functions
-
-	teardown_database(connection_string)
-
-	os.Exit(exitVal) //
+	defer os.Exit(exitVal)
 
 	// the above comments mean that I still need to reset the database after every test
 	// but not necessarily set them up again.
@@ -55,19 +175,19 @@ func TestCreateUser(t *testing.T) {
 	}
 
 	// run tests
-	tx, err := db.Begin()
-	// will be called after this function ends.
-	// 	No return statement means function returns at end
-	defer fmt.Println("called rollback") // delete this later
-	defer tx.Rollback()
-
-	if err != nil {
-		t.Errorf("Error in initializing transaction: %v", err)
-	}
-
-	// make transaction
+	userRepo := repository.NewStorage(db)
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			// make transaction
+			tx, err := db.Begin()
+			if err != nil {
+				t.Errorf("Error in initializing transaction: %v", err)
+			}
+			// will be called after this function ends.
+			// 	No return statement means function returns at end
+			defer fmt.Println("called rollback") // delete this later
+			defer tx.Rollback()
+
 			uID, err := userRepo.CreateUser(test.userRequest)
 
 			if err != test.want_error {
